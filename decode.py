0d554fbf6ab8adb0ce2cfc51202b5c41efd8b270
Foone Turing <fooneturing@gmail.com>
2018-09-21 09:09:37 -0700

    Support comments, skip metadata, better Py3 errors

--- decode.py
+++ decode.py
@@ -1,4 +1,8 @@
-import sys,csv,configparser
+#!/usr/bin/python3
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("This code is intended for Python 3")
+import csv,configparser
 
 def decode(s):
 	d=0x80
@@ -11,13 +15,15 @@ def decode(s):
 	return ''.join([chr(x) for x in out][5:])
 
 
-config = configparser.ConfigParser(strict=False)
+config = configparser.ConfigParser(strict=False, comment_prefixes= ('#', ';', '//'))
 config.read('setup.ini',
             encoding='Windows-1252')
 with open('results.csv','w',newline='') as f:
 	csvw=csv.writer(f)
 	csvw.writerow(['Name','Boxed','Jewelcase'])
 	for section in config.sections():
+		if section.startswith('!'):
+			continue
 		parts=[section,'','']
 		print('Software:',section)
 		for stype,si,opt in (('Boxed',1,'Bpassword'),('Jewelcase',2,'Jpassword')):
